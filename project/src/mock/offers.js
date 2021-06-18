cbfac173330416d31b861cc0634da4f258b43532
kalashnikovsb <kalashnikov.sk8@gmail.com>
2021-06-18 18:29:43 +0700

    Установил leaflet

--- project/src/mock/offers.js
+++ project/src/mock/offers.js
@@ -21,11 +21,11 @@ const offers = [
     'images': ['apartment-01.jpg', 'apartment-02.jpg', 'apartment-03.jpg'],
     'isFavorite': true,
     'isPremium': false,
-    // 'location': {
-    //   'latitude': 52.35514938496378,
-    //   'longitude': 4.673877537499948,
-    //   'zoom': 8
-    // },
+    'location': {
+      'latitude': 52.3909553943508,
+      'longitude': 4.85309666406198,
+      'zoom': 8,
+    },
     'maxAdults': 1,
     'previewImage': 'room-small.jpg',
     'price': 110,
@@ -55,11 +55,11 @@ const offers = [
     'images': ['apartment-02.jpg', 'apartment-03.jpg', 'apartment-01.jpg'],
     'isFavorite': false,
     'isPremium': true,
-    // 'location': {
-    //   'latitude': 52.35514938496378,
-    //   'longitude': 4.673877537499948,
-    //   'zoom': 8
-    // },
+    'location': {
+      'latitude': 52.369553943508,
+      'longitude': 4.85309666406198,
+      'zoom': 8
+    },
     'maxAdults': 2,
     'previewImage': 'room-small.jpg',
     'price': 220,
@@ -89,11 +89,11 @@ const offers = [
     'images': ['apartment-03.jpg', 'apartment-01.jpg', 'apartment-02.jpg'],
     'isFavorite': true,
     'isPremium': false,
-    // 'location': {
-    //   'latitude': 52.35514938496378,
-    //   'longitude': 4.673877537499948,
-    //   'zoom': 8
-    // },
+    'location': {
+      'latitude': 52.3909553943508,
+      'longitude': 4.929309666406198,
+      'zoom': 8
+    },
     'maxAdults': 3,
     'previewImage': 'room-small.jpg',
     'price': 330,
@@ -123,11 +123,11 @@ const offers = [
     'images': ['apartment-01.jpg', 'apartment-02.jpg', 'apartment-03.jpg'],
     'isFavorite': false,
     'isPremium': true,
-    // 'location': {
-    //   'latitude': 52.35514938496378,
-    //   'longitude': 4.673877537499948,
-    //   'zoom': 8
-    // },
+    'location': {
+      'latitude': 52.3809553943508,
+      'longitude': 4.939309666406198,
+      'zoom': 8
+    },
     'maxAdults': 4,
     'previewImage': 'room-small.jpg',
     'price': 440,
